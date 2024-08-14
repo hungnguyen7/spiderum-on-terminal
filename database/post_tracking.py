e6d4dec5d6644a60b1fe30233d57df0c2c7b8b1f
Hung Nguyen <hungnguyen.cosc@gmail.com>
2024-08-14 14:04:57 +0700

    #feat: improve codebase

--- database/post_tracking.py
+++ database/post_tracking.py
@@ -1,23 +1,25 @@
 from database.base_connection import BaseConnection
 
+
 class PostTracking(BaseConnection):
     """
     This class is responsible for managing the post_tracking collection.
     It provides methods to insert, mark as read and check if a post is read.
-    
+
     Attributes:
         collection_name (str): the name of the collection
     """
+
     def __init__(self):
         super().__init__(collection_name='post_tracking')
-        
+
     def insert_post(self, slug):
         """
         Insert a post in the post_tracking collection.
-        
+
         Args:
             slug (str): the slug of the post
-            
+
         Returns:
             None
         """
@@ -27,10 +29,10 @@ class PostTracking(BaseConnection):
     def mark_post_as_read(self, slug):
         """
         Mark a post as read in the post_tracking collection.
-        
+
         Args:
             slug (str): the slug of the post
-            
+
         Returns:
             None
         """
@@ -39,10 +41,10 @@ class PostTracking(BaseConnection):
     def is_post_read(self, slug):
         """
         Check if a post is read in the post_tracking collection.
-        
+
         Args:
             slug (str): the slug of the post
-        
+
         Returns:
             bool: True if the post is read, False otherwise
         """
