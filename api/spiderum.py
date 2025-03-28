f527ba8393c1d26b1d87c1576c6faa88334de717
Hung Nguyen <hungnguyen.cosc@gmail.com>
2025-03-28 11:14:03 +0700

    #feat: retry when connection error occur

--- api/spiderum.py
+++ api/spiderum.py
@@ -1,4 +1,6 @@
 import requests
+import time
+
 
 class SpiderumAPI:
     """
@@ -24,11 +26,16 @@ class SpiderumAPI:
         Returns:
             list: A list of posts
         """
-        url = cls.FEED_URL.format(page_idx=page_idx)
-        print("Getting data from:", url)
-        response = requests.get(url, verify=False, timeout=10)
-        response.raise_for_status()
-        return response.json()['posts']["items"]
+        try:
+            url = cls.FEED_URL.format(page_idx=page_idx)
+            print("Getting data from:", url)
+            response = requests.get(url, verify=False, timeout=10)
+            response.raise_for_status()
+            return response.json()['posts']["items"]
+        except requests.exceptions.ConnectionError:
+            print("ConnectionError: retrying in 5 seconds")
+            time.sleep(5)
+            return cls.fetch_posts(page_idx)
 
     @classmethod
     def fetch_post_content(cls, slug):
@@ -41,7 +48,12 @@ class SpiderumAPI:
         Returns:
             dict: The post content
         """
-        url = cls.POST_URL + slug
-        response = requests.get(url, verify=False, timeout=10)
-        response.raise_for_status()
-        return response.json()["post"]
+        try:
+            url = cls.POST_URL + slug
+            response = requests.get(url, verify=False, timeout=10)
+            response.raise_for_status()
+            return response.json()["post"]
+        except requests.exceptions.ConnectionError:
+            print("ConnectionError: retrying in 5 seconds")
+            time.sleep(5)
+            return cls.fetch_post_content(slug)
